5c9e93f73aa9965758ec7397c0e6165c786f4a93
BIYONGJIN_GLB <BIYONGJIN_GLB_glb@163.com>
2020-03-11 15:30:19 +0800

    c

--- src/test/java/com/test/Test1.java
+++ src/test/java/com/test/Test1.java
@@ -5,6 +5,7 @@ public static void main(String[] args) {
 	System.out.println("2222aaaaa");
 	System.out.println("2222sdsds");
 	System.out.println("hot fix");
+	System.out.println("hot fix2");
 
 }
 }
